71d439daae6c41fe900731b072fc343813e3c3fb
Joshua Voss <joshuavoss@gmail.com>
2023-03-30 22:09:17 -0700

    Update ETLight.py
    asked chat-gpt to add some functionality like converting time zones and this was the result pretty good but id like to clean up the edges still as it's not pretty data. 
    
    CHATGPT when i showed it my original source code.
    
    This is a Python code that reads data from a Google Sheet and saves it as a CSV file.
    
    Here's a brief summary of what the code does:
    
    Imports the Pandas library as "pd".
    Creates an empty DataFrame named "df".
    Defines two variables, "sheet_id" and "sheet_name", and assigns empty values to them.
    Constructs a URL string that points to the Google Sheet that contains the data to be read.
    Uses the Pandas read_csv() function to read the data from the specified URL and assigns it to the DataFrame "df".
    Formats the column names by capitalizing the first letter of each word, replacing spaces with underscores, and removing any non-alphanumeric characters from the names.
    Replaces any non-alphanumeric characters with a space in the DataFrame values.
    Displays the DataFrame in a Jupyter notebook or other compatible environment.
    Writes the DataFrame to a CSV file named "my_file.csv" with UTF-8 encoding and quoting of all fields.
    Overall, the code reads data from a Google Sheet, cleans and formats the data, and saves it as a CSV file for further analysis or processing.
    
    it got that right so i asked it to 
    
    id like this program to be able to detect if a column is a time stamp based on the text and then have a variable for source time zone and a variable for output time that would add a column to the google sheet and csv when saving that has the converted time a example would be UTC to new york time. 
    
    and it saved me a ton of time

--- ETLight.py
+++ ETLight.py
@@ -1,12 +1,49 @@
 import pandas as pd
+from datetime import datetime
+import pytz
+import csv
+
 df = pd.DataFrame()
-sheet_id = ''
-sheet_name = ''
+sheet_id = '1bNuRvFZHQGvprpVO1Vag0LgJRkSzjkiZV4XSrvzcVLQ'
+sheet_name = 'Sheet1'
 url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
 df = pd.read_csv(url)
+
+# Check if a column is a timestamp
+def is_timestamp(col):
+    try:
+        pd.to_datetime(col)
+        return True
+    except ValueError:
+        return False
+
+# Convert timestamp column to another timezone
+def convert_timezone(col, src_timezone, dst_timezone):
+    tz_src = pytz.timezone(src_timezone)
+    tz_dst = pytz.timezone(dst_timezone)
+    return pd.to_datetime(col, utc=True).dt.tz_convert(tz_dst).dt.tz_localize(None)
+
+# Add column with today's date
+today = datetime.today().strftime('%Y-%m-%d')
+df[today] = pd.to_datetime(today)
+
+# Detect timestamp columns and convert timezone
+for col in df.columns:
+    if is_timestamp(df[col]):
+        df[f'{col}_local'] = convert_timezone(df[col], 'UTC', 'America/New_York')
+
+# Format column names
 df.columns = map(str.title, df.columns)
 df.columns = df.columns.str.replace(' ','_')
 df.columns = df.columns.str.replace('[^a-zA-Z_]', '', regex=True)
+
+# Replace any non-alphanumeric characters with a space in the DataFrame values.
 df = df.replace('[^0-9a-zA-Z,_-]+', ' ', regex=True)
+
+# Display the DataFrame
 display(df)
+
+# Write the DataFrame to a CSV file with time zone conversion and today's date column
+output_timezone = 'America/New_York'  # Output time zone
 df.to_csv('my_file.csv', index=False, quoting=csv.QUOTE_ALL, quotechar='"', encoding='utf-8')
+
