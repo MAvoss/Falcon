4abba840f5d4d042ae6a5a7b3b368dc5f1c8c6e7
Joshua Voss <joshuavoss@gmail.com>
2023-04-01 04:56:52 -0700

    Update Compressor.py
    fixed ui because chat gpt sucks at ui design

--- Compressor.py
+++ Compressor.py
@@ -12,23 +12,33 @@ class FileCompressorGUI(tk.Frame):
         self.pack()
         self.create_widgets()
         self.compressed_files = []
+        self.load_compressed_files()
+        
+    
 
     def create_widgets(self):
-        # Ask user for file to compress
-        self.file_button = tk.Button(self, text="Select file to compress", command=self.choose_file)
+         # Ask user for file to compress
+        def select_and_compress():
+            self.choose_file()
+            self.compress_file()
+
+        self.file_button = tk.Button(self, text="Select file to compress", command=select_and_compress)
         self.file_button.pack()
+        # Ask user for file to compress
+        #self.file_button = tk.Button(self, text="Select file to compress", command=self.choose_file)
+        #self.file_button.pack()
 
         # Compress and save file as Base64-encoded string in JSON file
-        self.compress_button = tk.Button(self, text="Compress file", command=self.compress_file)
-        self.compress_button.pack()
-
-        # Ask user for JSON file to decompress
-        self.json_button = tk.Button(self, text="Select JSON file", command=self.choose_json)
-        self.json_button.pack()
+        #self.compress_button = tk.Button(self, text="Compress file", command=self.compress_file)
+        #self.compress_button.pack()
 
         # Show list of compressed files in JSON file
         self.file_listbox = tk.Listbox(self)
         self.file_listbox.pack()
+
+        # Ask user for JSON file to decompress
+        #self.json_button = tk.Button(self, text="Select JSON file", command=self.choose_json)
+        #self.json_button.pack()
 
         # Decompress selected file from JSON file
         self.decompress_button = tk.Button(self, text="Decompress selected file", command=self.decompress_file)
@@ -44,14 +54,13 @@ class FileCompressorGUI(tk.Frame):
         self.compressed_files.append(data)
         with open("compressed_files.json", "w") as f:
             json.dump(self.compressed_files, f)
+        self.update_listbox()
 
     def choose_json(self):
-        self.json_filepath = filedialog.askopenfilename()
+        self.json_filepath = "compressed_files.json"
         with open(self.json_filepath, "r") as f:
             self.compressed_files = json.load(f)
-        self.file_listbox.delete(0, tk.END)
-        for file in self.compressed_files:
-            self.file_listbox.insert(tk.END, file["filename"])
+        self.update_listbox()
 
     def decompress_file(self):
         selected_index = self.file_listbox.curselection()
@@ -61,6 +70,19 @@ class FileCompressorGUI(tk.Frame):
             filename = selected_file["filename"]
             with open(filename, "wb") as f:
                 f.write(zlib.decompress(compressed_data))
+
+    def load_compressed_files(self):
+        try:
+            with open("compressed_files.json", "r") as f:
+                self.compressed_files = json.load(f)
+                self.update_listbox()
+        except FileNotFoundError:
+            pass
+
+    def update_listbox(self):
+        self.file_listbox.delete(0, tk.END)
+        for file in self.compressed_files:
+            self.file_listbox.insert(tk.END, file["filename"])
 
 root = tk.Tk()
 app = FileCompressorGUI(master=root)
