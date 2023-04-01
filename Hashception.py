9f1a4eeae05bde0649701d650b23af072fa3b77f
Joshua Voss <joshuavoss@gmail.com>
2023-03-31 18:41:45 -0700

    Create Hashception.py
    Ipfs archiving tool with weird way of thinking of history

---
+++ Hashception.py
@@ -0,0 +1,4 @@
+#a program to write text and or optionally any file to ipfs then save the hashes as a new line on a Json file. 
+#then the Json file will be sent to ipfs and the hah for that is all that will be returned when the files close
+#each time it saves with new data there will be a new hash so I'll store that old one as well as a time capsule. 
+Basically version controller. 
