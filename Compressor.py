2a4a94da10ee242b839a50ac26ea0de98f4af7b2
Joshua Voss <joshuavoss@gmail.com>
2023-04-01 04:36:33 -0700

    Create Compressor.py
    this takes a file converts it to text in json and give you the ability to extract the file from the text hash

---
+++ Compressor.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from tkinter import filedialog
+import json
+import zlib
+import base64
+
+class FileCompressorGUI(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.master.title("File Compressor")
+        self.pack()
+        self.create_widgets()
+        self.compressed_files = []
+
+    def create_widgets(self):
+        # Ask user for file to compress
+        self.file_button = tk.Button(self, text="Select file to compress", command=self.choose_file)
+        self.file_button.pack()
+
+        # Compress and save file as Base64-encoded string in JSON file
+        self.compress_button = tk.Button(self, text="Compress file", command=self.compress_file)
+        self.compress_button.pack()
+
+        # Ask user for JSON file to decompress
+        self.json_button = tk.Button(self, text="Select JSON file", command=self.choose_json)
+        self.json_button.pack()
+
+        # Show list of compressed files in JSON file
+        self.file_listbox = tk.Listbox(self)
+        self.file_listbox.pack()
+
+        # Decompress selected file from JSON file
+        self.decompress_button = tk.Button(self, text="Decompress selected file", command=self.decompress_file)
+        self.decompress_button.pack()
+
+    def choose_file(self):
+        self.filepath = filedialog.askopenfilename()
+
+    def compress_file(self):
+        with open(self.filepath, "rb") as f:
+            compressed_data = zlib.compress(f.read())
+        data = {"filename": self.filepath.split("/")[-1], "data": base64.b64encode(compressed_data).decode("utf-8")}
+        self.compressed_files.append(data)
+        with open("compressed_files.json", "w") as f:
+            json.dump(self.compressed_files, f)
+
+    def choose_json(self):
+        self.json_filepath = filedialog.askopenfilename()
+        with open(self.json_filepath, "r") as f:
+            self.compressed_files = json.load(f)
+        self.file_listbox.delete(0, tk.END)
+        for file in self.compressed_files:
+            self.file_listbox.insert(tk.END, file["filename"])
+
+    def decompress_file(self):
+        selected_index = self.file_listbox.curselection()
+        if selected_index:
+            selected_file = self.compressed_files[selected_index[0]]
+            compressed_data = base64.b64decode(selected_file["data"])
+            filename = selected_file["filename"]
+            with open(filename, "wb") as f:
+                f.write(zlib.decompress(compressed_data))
+
+root = tk.Tk()
+app = FileCompressorGUI(master=root)
+app.mainloop()
